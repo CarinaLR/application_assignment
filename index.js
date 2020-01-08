305b13e87efa30e45d03599f352d2845343c8ff7
CarinaLR <carinalr84@gmail.com>
2020-01-08 17:33:36 -0500

    adding button and data

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
+'use strict';
+
 const express = require('express');
-const app = express();
+const path = require('path');
 const port = 3000;
 
-app.use(express.static('public'));
+const app = express();
 
-app.get('/quote', (req, res) => {
-  res.json({
-    quote: quotes.getQuote()
-  });
+// body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// static middleware
+app.use(express.static('./public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+}); // Send index.html for any other requests
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
 });
 
+// app.get('/quote', (req, res) => {
+//   res.json({
+//     quote: quotes.getQuote(),
+//   });
+// });
 
 app.listen(port, () => console.log(`Awesome app listening on port ${port}!`));
+
+module.exports = app;
--- public/main.js
+++ public/main.js
@@ -3,3 +3,38 @@ fetch('/quote')
   .then(data => {
     document.getElementById('quote').innerText = data.quote;
   });
+
+const data = [
+  "It's just a flesh wound.",
+  'I am just one stomach flu away from my goal weight.',
+  'I am serious.',
+  'That is my least vulnerable spot.',
+  'We get the warhead and we hold the world ransom for…. One million dollars.',
+];
+
+function provideQuote() {
+  const Quote1 = shuffle(data);
+  document.querySelector('#quote > h2').innerHTML = Quote1;
+  return Quote1;
+}
+
+function shuffle(data) {
+  for (let i = data.length - 1; i > 0; i--) {
+    let randomIndex = Math.floor(Math.random() * (i + 1));
+    let temp = data[i];
+    data[i] = data[randomIndex];
+    data[randomIndex] = temp;
+  }
+  return data;
+}
+
+function playGame() {
+  //storage anspecific element fromt he html file and link it to a addEventListener.
+  const playAgain = document.getElementById('playAgain');
+
+  playAgain.addEventListener('click', function() {
+    shuffle();
+  });
+}
+// start up the game!
+playGame();
